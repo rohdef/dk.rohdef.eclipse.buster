3d75495a2baeddd91207af7fb4982b0881452c1c
Rohde Fischer <rohdef@rohdef.dk>
2013-05-23 12:26:17 +0200

    Parses the entire test xml correctly

--- src/dk/rohdef/eclipse/buster/models/Failure.java
+++ src/dk/rohdef/eclipse/buster/models/Failure.java
@@ -1,5 +1,42 @@
 package dk.rohdef.eclipse.buster.models;
 
-public class Failure {
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlValue;
 
+public class Failure {
+	private String type, message, text;
+
+	public Failure() {
+		this(null, null, null);
+	}
+	public Failure(String type, String message, String text) {
+		super();
+		this.type = type;
+		this.message = message;
+		this.text = text;
+	}
+
+	@XmlAttribute
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	@XmlAttribute
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	@XmlValue
+	public String getText() {
+		return text;
+	}
+	public void setText(String text) {
+		this.text = text;
+	}
 }
